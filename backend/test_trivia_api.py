b25ff26d9abb393059f63204dabcf5c5a14f7d2a
Fitzy <fitzystrikesagain@gmail.com>
2021-07-02 18:58:58 -0500

    add test for question creation

--- backend/test_trivia_api.py
+++ backend/test_trivia_api.py
@@ -37,15 +37,20 @@ class TriviaTestCase(unittest.TestCase):
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
             self.db.create_all()
-            # print(self.app.url_map)
 
-        new_question = Question(
-            question=TEST_QUESTION,
-            answer="Marie Curie",
-            difficulty=3,
-            category="4"
-        )
-        new_question.insert()
+        self.new_question = {
+            "question": TEST_QUESTION,
+            "answer": "Marie Curie",
+            "difficulty": 3,
+            "category": "4"
+        }
+
+        Question(
+            question=self.new_question["question"],
+            answer=self.new_question["answer"],
+            difficulty=self.new_question["difficulty"],
+            category=self.new_question["category"],
+        ).insert()
 
     def tearDown(self):
         questions = Question.query.filter(Question.question == TEST_QUESTION)
@@ -54,17 +59,31 @@ class TriviaTestCase(unittest.TestCase):
 
     def get_endpoints_helper(self, endpoint, status_code=200):
         """
-        {endpoint} should return 200
+        GET {endpoint} should return 200
         """
         print(self.get_endpoints_helper.__doc__.format(endpoint=endpoint))
         res = self.client().get(endpoint)
         return res.status_code == status_code
 
     def test_categories_endpoint(self):
-        self.assertTrue(self.get_endpoints_helper(endpoint="/categories"))
+        """
+        GET /categories should return 200
+        """
 
     def test_questions_endpoint(self):
+        """
+        GET /questions should return 200
+        """
+        self.assertTrue(self.get_endpoints_helper(endpoint="/categories"))
         self.assertTrue(self.get_endpoints_helper(endpoint="/questions"))
+
+    def test_create_question(self):
+        """
+        POST /questions should return 200
+        """
+        print(self.test_create_question.__doc__)
+        res = self.client().post("/questions", json=self.new_question)
+        self.assertEqual(res.status_code, 200)
 
 
 if __name__ == "main":
