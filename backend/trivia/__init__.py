22730a028378de0bcda14ed1383a174fe25bad8b
Fitzy <fitzystrikesagain@gmail.com>
2021-07-02 01:22:05 -0500

    implement GET /categories endpoint

--- backend/trivia/__init__.py
+++ backend/trivia/__init__.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -23,13 +25,18 @@ def create_app(test_config=None):
                              "GET,PUT,PATCH,POST,DELETE,OPTIONS")
         return response
 
-    '''
-    @TODO:
-    Create an endpoint to handle GET requests
-    for all available categories.
-    '''
+    @app.route("/categories", methods=["GET"])
+    def get_categories():
+        """
+        Create an endpoint to handle GET requests for all available categories.
+        """
+        cat_list = Category.query.order_by(Category.id).all()
+        return jsonify({
+            "status": "success",
+            "categories": [c.format() for c in cat_list]
+        })
 
-    '''
+    """
     @TODO:
     Create an endpoint to handle GET requests for questions,
     including pagination (every 10 questions).
@@ -41,9 +48,9 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for
     three pages.
     Clicking on the page numbers should update the questions.
-    '''
+    """
 
-    '''
+    """
     @TODO:
     Create an endpoint to DELETE question using a question ID.
 
@@ -51,9 +58,9 @@ def create_app(test_config=None):
     will
     be removed. This removal will persist in the database and when you
     refresh the page.
-    '''
+    """
 
-    '''
+    """
     @TODO:
     Create an endpoint to POST a new question,
     which will require the question and answer text,
@@ -62,9 +69,9 @@ def create_app(test_config=None):
     TEST: When you submit a question on the "Add" tab,
     the form will clear and the question will appear at the end of the last
     page of the questions list in the "List" tab.
-    '''
+    """
 
-    '''
+    """
     @TODO:
     Create a POST endpoint to get questions based on a search term.
     It should return any questions for whom the search term
@@ -73,18 +80,18 @@ def create_app(test_config=None):
     TEST: Search by any phrase. The questions list will update to include
     only question that include that string within their question.
     Try using the word "title" to start.
-    '''
+    """
 
-    '''
+    """
     @TODO:
     Create a GET endpoint to get questions based on category.
 
     TEST: In the "List" tab / main screen, clicking on one of the
     categories in the left column will cause only questions of that
     category to be shown.
-    '''
+    """
 
-    '''
+    """
     @TODO:
     Create a POST endpoint to get questions to play the quiz.
     This endpoint should take category and previous question parameters
@@ -94,12 +101,12 @@ def create_app(test_config=None):
     TEST: In the "Play" tab, after a user selects "All" or a category,
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
-    '''
+    """
 
-    '''
+    """
     @TODO:
     Create error handlers for all expected errors
     including 404 and 422.
-    '''
+    """
 
     return app
