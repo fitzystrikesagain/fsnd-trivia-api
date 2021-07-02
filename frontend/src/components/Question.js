c1e44b855aeca0b07c50aa94fa2cf5a6efaf2c4a
Fitzy <fitzystrikesagain@gmail.com>
2021-07-02 12:40:08 -0500

    fix image links and formatting

--- frontend/src/components/Question.js
+++ frontend/src/components/Question.js
@@ -1,39 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../stylesheets/Question.css';
 
 class Question extends Component {
-  constructor(){
-    super();
-    this.state = {
-      visibleAnswer: false
+    constructor() {
+        super();
+        this.state = {
+            visibleAnswer: false
+        }
     }
-  }
 
-  flipVisibility() {
-    this.setState({visibleAnswer: !this.state.visibleAnswer});
-  }
+    flipVisibility() {
+        this.setState({visibleAnswer: !this.state.visibleAnswer});
+    }
 
-  render() {
-    const { question, answer, category, difficulty } = this.props;
-    return (
-      <div className="Question-holder">
-        <div className="Question">{question}</div>
-        <div className="Question-status">
-          <img className="category" src={`${category.toLowerCase()}.svg`}/>
-          <div className="difficulty">Difficulty: {difficulty}</div>
-          <img src="delete.png" className="delete" onClick={() => this.props.questionAction('DELETE')}/>
-          
-        </div>
-        <div className="show-answer button"
-            onClick={() => this.flipVisibility()}>
-            {this.state.visibleAnswer ? 'Hide' : 'Show'} Answer
-          </div>
-        <div className="answer-holder">
-          <span style={{"visibility": this.state.visibleAnswer ? 'visible' : 'hidden'}}>Answer: {answer}</span>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        let {question, answer, category, difficulty} = this.props;
+        // TODO: fix undefined for category #6
+        if (typeof category === "undefined") {
+            category = "sports";
+        }
+        return (
+            <div className="Question-holder">
+                <div className="Question">{question}</div>
+                <div className="Question-status">
+                    <img className="category"
+                         src={`../../${category.toLowerCase()}.svg`}/>
+                    <div className="difficulty">Difficulty: {difficulty}</div>
+                    <img alt="rubbish bin" src="../../delete.png"
+                         className="delete"
+                         onClick={() => this.props.questionAction('DELETE')}/>
+
+                </div>
+                <div className="show-answer button"
+                     onClick={() => this.flipVisibility()}>
+                    {this.state.visibleAnswer ? 'Hide' : 'Show'} Answer
+                </div>
+                <div className="answer-holder">
+                    <span
+                        style={{"visibility": this.state.visibleAnswer ? 'visible' : 'hidden'}}>Answer: {answer}</span>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Question;
